5833306b74c1de921afc241ab72f0802c5e76130
Willi Ye <williye97@gmail.com>
2016-08-01 02:00:00 +0800

    Add lmk kill

--- app/src/main/java/com/grarak/kerneladiutor/utils/kernel/lmk/LMK.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/kernel/lmk/LMK.java
@@ -36,6 +36,32 @@ public class LMK {
 
     private static final String MINFREE = "/sys/module/lowmemorykiller/parameters/minfree";
     private static final String ADAPTIVE = "/sys/module/lowmemorykiller/parameters/enable_adaptive_lmk";
+    private static final String SWAP_WAIT = "/sys/module/lowmemorykiller/parameters/swap_wait";
+    private static final String SWAP_WAIT_PERCENT = "/sys/module/lowmemorykiller/parameters/swap_wait_percent";
+
+    public static void setSwapWaitPercent(int value, Context context) {
+        run(Control.write(String.valueOf(value), SWAP_WAIT_PERCENT), SWAP_WAIT_PERCENT, context);
+    }
+
+    public static int getSwapWaitPercent() {
+        return Utils.strToInt(Utils.readFile(SWAP_WAIT_PERCENT));
+    }
+
+    public static boolean hasSwapWaitPercent() {
+        return Utils.existFile(SWAP_WAIT_PERCENT);
+    }
+
+    public static void enableSwapWait(boolean enable, Context context) {
+        run(Control.write(enable ? "Y" : "N", SWAP_WAIT), SWAP_WAIT, context);
+    }
+
+    public static boolean isSwapWaitEnabled() {
+        return Utils.readFile(SWAP_WAIT).equals("Y");
+    }
+
+    public static boolean hasSwapWait() {
+        return Utils.existFile(SWAP_WAIT);
+    }
 
     public static void setMinFree(String value, Context context) {
         run(Control.chmod("644", MINFREE), MINFREE + "chmod444", context);
